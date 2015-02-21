7da78d5103f54725040ec9009a8e6e627846e4d0
Jorge Saud <jorgelsaud@gmail.com>
2015-02-21 11:36:32 -0430

    anadidos los componentes css de bootstrap via bower y algunos otros necesarios

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,29 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+ mix.copy('vendor/bower_components/bootstrap-sass-official/assets/stylesheets', 'resources/assets/sass/bootstrap')
+     .copy('vendor/bower_components/bootstrap-sass-official/assets/images', 'resources/assets/sass/bootstrap')
+     .copy('vendor/bower_components/bootstrap-sass-official/assets/fonts', 'public/assets/fonts')
+     .copy('vendor/bower_components/jquery/dist', 'resources/js/jquery')
+     .copy('vendor/bower_components/bootstrap-sass-official/assets/javascripts', 'resources/js/bootstrap')
+     .copy('vendor/bower_components/bootstrap-datepicker/build','resources/assets/less/bootstrap-datepicker/buid')
+     .copy('vendor/bower_components/bootstrap-datepicker/less','resources/assets/less/bootstrap-datepicker/less')
+     .copy('vendor/bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js','resources/js/bootstrap-datepicker/bootstrap-datepicker.js')
+     .copy('vendor/bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.es.js','resources/js/bootstrap-datepicker/bootstrap-datepicker.es.js')
+
+     .sass('sass.scss', 'resources/css')
+     .less('less.less', 'resources/css')
+     .scripts([
+      'jquery/jquery.js',
+      'bootstrap/bootstrap.js',
+      'bootstrap-datepicker/bootstrap-datepicker.js',
+      'bootstrap-datepicker/bootstrap-datepicker.es.js',
+      'main.js'
+     ])
+     .styles([
+      'sass.css',
+      'less.css'
+     ])
+     .version(['css/all.css', 'js/all.js'])
+
 });
