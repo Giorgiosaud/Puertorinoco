c9e1d0194d53622ecdc34b54ff4931b635e968b7
Jorge Saud <jorgelsaud@gmail.com>
2015-02-23 11:22:09 -0430

    logo anadido

--- database/seeds/nuevasFunccionesSeed.php
+++ database/seeds/nuevasFunccionesSeed.php
@@ -1,13 +1,13 @@
 <?php
 //require "fzaninotto/faker"
+use App\Variable;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
 class NuevasFunccionesSeeder extends Seeder {
 
     public function run()
     {
-
+        Variable::create('nombre'=>'logo','valor'=>'uploads/logo100.png');
     }
 
 }
