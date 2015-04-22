e5aaabc7e01632c77f0659937e6e16b28473b169
Jorge Saud <jorgelsaud@gmail.com>
2015-04-21 20:40:03 -0430

    p

--- app/Http/Controllers/MercadopagoController.php
+++ app/Http/Controllers/MercadopagoController.php
@@ -10,8 +10,9 @@ class MercadopagoController extends Controller {
 
     public function success(Request $request)
     {
-        // dd($request->all());
+         dd($request->all());
         // dd(\Mercadopago::get_access_token());
+
         $d = Mercadopago::get_payment($request->input('collection_id'));
         dd($d['response']);
         if ($d['status'] == 200)
