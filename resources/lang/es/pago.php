574c051f9a41d8ed539e2b4085ee8f6c2abe36e0
Jorge Saud <jorgelsaud@gmail.com>
2015-04-21 21:30:34 -0430

    mp

---
+++ resources/lang/es/pago.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by jorgelsaud.
+ * User: jorgelsaud
+ * Date: 21/4/15
+ * Time: 21:29
+ */
+return[
+    'aceptado'=>'Su Pago Fue Aceptado y registrado Muchas Gracias',
+    'sinConeccion'=>'El Host no Responde por favor intente luego',
+];
