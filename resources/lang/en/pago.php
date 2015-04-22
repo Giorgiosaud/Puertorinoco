e6c998c2205676dcd3c99009cd7e8db39881f153
Jorge Saud <jorgelsaud@gmail.com>
2015-04-21 21:34:21 -0430

    mp

---
+++ resources/lang/en/pago.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by jorgelsaud.
+ * User: jorgelsaud
+ * Date: 21/4/15
+ * Time: 21:29
+ */
+return[
+    'aceptado'=>'Your Payment Was accepted and registered',
+    'sinConeccion'=>'We can\'t connect with host',
+];
---
+++ resources/lang/pt-BR/pago.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by jorgelsaud.
+ * User: jorgelsaud
+ * Date: 21/4/15
+ * Time: 21:29
+ */
+return[
+    'aceptado'=>'Seu pagamento foi aceite e registada',
+    'sinConeccion'=>'Nós não consegue se conectar com o anfitrião',
+];
