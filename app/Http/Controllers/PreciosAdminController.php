177f9686335dec2a47db4b6b0635de031e01dca1
Jorge Saud <jorgelsaud@gmail.com>
2015-05-05 21:04:03 -0430

    update

--- app/Http/Controllers/PreciosAdminController.php
+++ app/Http/Controllers/PreciosAdminController.php
@@ -55,7 +55,7 @@ class PreciosAdminController extends Controller {
 	 */
 	public function store(PrecioRequest $request)
 	{
-        Precio::create($request);
+        Precio::create($request->all());
         return redirect('PanelAdministrativo.precios.index');
 
 	}
