d353ac86647f0a63817c0f15b903c18cb977d59a
Jorge Saud <jorgelsaud@gmail.com>
2017-03-20 17:38:20 -0400

    fix paseos

--- resources/views/TiposDePaseos/admin/create.blade.php
+++ resources/views/TiposDePaseos/admin/create.blade.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<h1>Crear Tipo de Paseo</h1>
 		{!!Form::horizontal(['url'=>URL::route('PanelAdministrativo.tipoDePaseo.store'),'role'=>'form'])!!}
-		@include('Paseos.admin.partials._form',['submit'=>'Crear Tipo de Paseo'])
+		@include('TiposDePaseos.admin.partials._form',['submit'=>'Crear Tipo de Paseo'])
 		{!!Form::close()!!}
 
 	</div>
