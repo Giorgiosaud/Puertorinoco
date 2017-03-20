b18fd141e204d3f239c5836533a44efe48995048
Jorge Saud <jorgelsaud@gmail.com>
2017-03-20 16:23:51 -0400

    testing dates

--- resources/views/Paseos/admin/partials/_form.blade.php
+++ resources/views/Paseos/admin/partials/_form.blade.php
@@ -28,7 +28,7 @@ Form::label('tipo_de_paseo_id', 'Tipo de Paseo: '),
 Form::select('tipo_de_paseo_id',$tiposDePaseos,null,['class'=>'form-control'])
 )
 !!}
-{{-- {!! var_dump($embarcaciones) !!} --}}
+{!! var_dump($paseo->embarcaciones) !!}
 {!!
 ControlGroup::generate(
 Form::label('lista_de_embarcaciones', 'Embarcaciones: '),
