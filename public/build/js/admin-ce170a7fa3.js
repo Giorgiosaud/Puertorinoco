080f896d882f21d0d79027b29afab0e92f494e69
Jorge Saud <jorgelsaud@gmail.com>
2017-03-28 08:54:37 -0400

    fecha ESpecial

--- public/build/js/admin-ce170a7fa3.js
+++ public/build/js/admin-ce170a7fa3.js
@@ -18110,6 +18110,7 @@ function fechasEspecialesx(fechaAComparar) {
     // console.log(fechaAComparar);
     for (i = 0; i < fechasEspeciales.length; i++) {
         var fechaEspecial = new Date(fechasEspeciales[i].fecha.date);
+        console.info('Probando si fecha '+fechaEspecial+' es especial al compararla con '+fechaAComparar );
         if(fechaEspecial===fechaAComparar){
             console.log('fechaEspecialCombinada '+fechaEspecial);
             console.info('fecha Especial Encontrada');
