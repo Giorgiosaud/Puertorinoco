a0b42dbc7a7e24a9fd73e8fef2a97f8e0b45f9d1
Jorge Saud <jorgelsaud@gmail.com>
2017-03-28 09:09:49 -0400

    fecha ESpecial

--- app/Http/Controllers/FechasEspecialesAdminController.php
+++ app/Http/Controllers/FechasEspecialesAdminController.php
@@ -104,7 +104,7 @@ class FechasEspecialesAdminController extends Controller {
 		$fechaEspecial->update($req->all());
 		$embarcaciones=$request->input('lista_de_embarcaciones');
 		foreach ($embarcaciones as $embaracionId){
-			$fechaEspecial->embarcaciones()->sync($embaracionId,array('activa'=>$request->input('trabaja')));
+			$fechaEspecial->embarcaciones()->sync($embaracionId,array('activa'=>$req->input('trabaja')));
 		}
 		return redirect()->route('PanelAdministrativo.fechasEspeciales.index');
 
