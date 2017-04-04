6367b91b2da5a695189853f108a7c9f9171508ee
Jorge Saud <jorgelsaud@gmail.com>
2017-04-04 17:23:31 -0400

    fecha especial revision

--- app/Http/Controllers/VariablesController.php
+++ app/Http/Controllers/VariablesController.php
@@ -329,7 +329,7 @@ class VariablesController extends Controller {
         $fechaEspecial=FechaEspecial::where('fecha',$fecha)->get();
         if(!($fechaEspecial->isEmpty()))
         {
-            // dd($fechaEspecial->embarcaciones->first()->activa);
+            dd($fechaEspecial->embarcaciones->first()->activa);
             if($fechaEspecial->embarcaciones->first()->activa==1){
                 return $embarcacion->paseos()->wherePublico(1)->orderBy('orden')->get();
             }
