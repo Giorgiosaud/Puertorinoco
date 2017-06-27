63c4e88f2d08f83f9a80e95f4e3738cae108ce64
Jorge Saud <jorgelsaud@gmail.com>
2017-06-27 08:37:15 -0400

    fix fechas especiales

--- resources/views/fechasEspeciales/admin/partials/_form.blade.php
+++ resources/views/fechasEspeciales/admin/partials/_form.blade.php
@@ -1,7 +1,13 @@
 <div class="input-group input-daterange">
-    <input name="fecha_inicio" type="text" class="form-control">
+    {!!	ControlGroup::generate(
+	Form::label('fecha_inicio', 'Fecha del Paseo: '),
+	Form::text('fecha_inicio',null,['class'=>"datePicker"])
+	)!!}
     <div class="input-group-addon">to</div>
-    <input name="fecha_fin" type="text" class="form-control" >
+    {!!	ControlGroup::generate(
+	Form::label('fecha_fin', 'Fecha del Paseo: '),
+	Form::text('fecha_fin',null,['class'=>"datePicker"])
+	)!!}
 </div>
 {!!	ControlGroup::generate(
 	Form::label('fecha', 'Fecha del Paseo: '),
