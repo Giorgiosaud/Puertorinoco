53a7c4faf095df5a4888eb7ef3ddb41866ad6840
Jorge Saud <jorgelsaud@gmail.com>
2018-01-04 12:39:50 -0300

    fix migration issue

---
+++ tests/NotificationsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class NotificationsTest extends TestCase {
+
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 * @test
+	 */
+	public function testBasicExample(){
+		// as a guest user
+
+		// visit a reservation page
+		$this->visit('en/reservation/create')
+			->assertSee('Date');
+		// should see an alert dismissable
+	}
+}
